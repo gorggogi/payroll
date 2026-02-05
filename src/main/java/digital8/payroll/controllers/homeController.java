cd2f4bd0308dca55ff3fdafbbaf2988836efba83
djqui <joannadenise.quinto.mnl@eac.edu.ph>
2026-02-05 09:59:51 +0800

    login initial

--- src/main/java/digital8/payroll/controllers/homeController.java
+++ src/main/java/digital8/payroll/controllers/homeController.java
@@ -2,9 +2,8 @@ package digital8.payroll.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class homeController {
@@ -16,6 +15,11 @@ public class homeController {
     public String adminHome() {
         return "html/homeAdmin";
     }
-    
-    
+
+    @PostMapping("/validateLogin")
+    public String validateLogin(Employees emp, RedirectAttributes redi) {
+        redi.addFlashAttribute("message", "User has been saved...");
+        service.save(employees);
+        return "html/homeEmployee";
+    }
 }
