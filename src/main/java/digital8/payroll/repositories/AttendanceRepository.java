bc62d5ba98a7183f73a8334f3a4ed08d608e59d4
k-ryll <andreskharyll@gmail.com>
2026-02-19 10:56:46 +0800

    Attendance function

--- src/main/java/digital8/payroll/repositories/AttendanceRepository.java
+++ src/main/java/digital8/payroll/repositories/AttendanceRepository.java
@@ -1,15 +1,17 @@
 package digital8.payroll.repositories;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import digital8.payroll.entities.Attendance;
-import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer>{
+	@Query("SELECT a FROM Attendance a WHERE a.employeeId = :employeeId ORDER BY a.attendance_date DESC")
+	List<Attendance> findByEmployeeIdOrderByDateDesc(@Param("employeeId") Integer employeeId);
 
-    
-    
 }
-    
+
 
