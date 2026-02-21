3c8f9de9f792ec4225b4f028e71fe0935a1e38c9
gorggogi <diligcharlene@gmail.com>
2026-02-21 11:07:30 +0800

    settings page draft

--- src/main/java/digital8/payroll/controllers/settingsController.java
+++ src/main/java/digital8/payroll/controllers/settingsController.java
@@ -1,12 +1,31 @@
 package digital8.payroll.controllers;
 
+import digital8.payroll.entities.Employees;
+import digital8.payroll.entities.Users;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class settingsController {
+    
     @GetMapping("/employee/settings")
-    public String forgotPasswordPage() {
-        return "html/settingsEmployee";
+    public String employeeSettingsPage  (Model model, Authentication authentication){
+        Object principal = authentication != null ? authentication.getPrincipal() : null;
+
+        if (principal instanceof Users) {
+            Users user = (Users) principal;
+            Employees emp = user.getEmployee();
+
+            if (emp != null){
+                model.addAttribute("employee", emp);
+                model.addAttribute("employeeName", emp.getFirstName() + " " + emp.getLastName());
+                model.addAttribute("user", user);
+            }
+         }
+
+         return "html/settingsEmployee";
     }
-}
+    }
+
